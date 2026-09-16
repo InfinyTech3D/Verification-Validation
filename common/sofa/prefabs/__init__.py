749ead417003fd8f9af614ac5a1c6868c4ecc9fc
Themis Skamagkis <themisskamagkis@gmail.com>
2026-09-16 16:42:59 +0200

    Add runaway forgotten dict

--- common/sofa/prefabs/__init__.py
+++ common/sofa/prefabs/__init__.py
@@ -1,3 +1,6 @@
 from .base import ScenePrefab
 from .bar import ElasticBar
 from .beam import ElasticBeam
+from ...geometry import Bar1D, Beam2D, Beam3D
+
+PREFAB_BY_GEOMETRY = {Bar1D: ElasticBar, Beam2D: ElasticBeam, Beam3D: ElasticBeam}
